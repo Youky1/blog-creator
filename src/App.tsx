da6236f07f472d134913ff5713b9899b4ff44331
Youky <1911303982@qq.com>
2022-06-29 13:15:39 +0800

    feat: init

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import "antd/dist/antd.css";
+import "./App.css";
+
+import Index from "./pages/Index";
+import Create from "./pages/Create";
+
+function App() {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="create" element={<Create />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.d.ts
@@ -0,0 +1,17 @@
+declare interface HeaderConfig {}
+declare interface AsideConfig {}
+declare interface FooterConfig {}
+
+interface BlogItem {
+  name: string;
+  isPage: boolean;
+  text: string;
+  children: Array<BlogItem>;
+}
+
+declare type Blog = Array<BlogItem>;
+
+declare interface FooterContent {
+  show: boolean;
+  text: string;
+}
